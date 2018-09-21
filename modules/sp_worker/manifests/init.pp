cfed2b0336259dd9901a0c14c20b2761bb2fdb57
irunika <uvoofLakmal075>
2018-09-21 09:42:57 +0530

    Template necessary configurations in worker

--- modules/sp_worker/manifests/init.pp
+++ modules/sp_worker/manifests/init.pp
@@ -27,7 +27,43 @@ class sp_worker (
   $start_script_template = $sp_worker::params::start_script_template,
   $deployment_yaml_template = $sp_worker::params::deployment_yaml_template,
   $jre_version = $sp_worker::params::jre_version,
-  $clustering = $sp_worker::params::clustering
+  $clustering = $sp_worker::params::clustering,
+
+  # -------- deployment.yaml configs --------
+
+  # listenerConfigurations
+  $default_host = $sp_worker::params::default_host,
+
+  $msf4j_host = $sp_worker::params::msf4j_host,
+  $msf4j_keystore_file = $sp_worker::params::msf4j_keystore_file,
+  $msf4j_keystore_password = $sp_worker::params::msf4j_keystore_password,
+  $msf4j_cert_pass = $sp_worker::params::msf4j_cert_pass,
+
+  # Configuration used for the databridge communication
+  $databridge_keystore_location = $sp_worker::params::databridge_keystore_location,
+  $databridge_keystore_password = $sp_worker::params::databridge_keystore_password,
+  $binary_data_receiver_hostname = $sp_worker::params::binary_data_receiver_hostname,
+
+  # Configuration of the Data Agents - to publish events through databridge
+  $thrift_agent_trust_store = $sp_worker::params::thrift_agent_trust_store,
+  $thrift_agent_trust_store_password = $sp_worker::params::thrift_agent_trust_store_password,
+  $binary_agent_trust_store = $sp_worker::params::binary_agent_trust_store,
+  $binary_agent_trust_store_password = $sp_worker::params::binary_agent_trust_store_password,
+
+  # Secure Vault Configuration
+  $securevault_private_key_alias = $sp_worker::params::securevault_private_key_alias,
+  $securevault_keystore = $sp_worker::params::securevault_keystore,
+  $securevault_secret_properties_file = $sp_worker::params::securevault_secret_properties_file,
+  $securevault_master_key_reader_file = $sp_worker::params::securevault_master_key_reader_file,
+
+  # Datasource Configurations
+  $carbon_db_rul = $sp_worker::params::carbon_db_rul,
+  $carbon_db_username = $sp_worker::params::carbon_db_username,
+  $carbon_db_password = $sp_worker::params::carbon_db_password,
+  $carbon_db_dirver = $sp_worker::params::carbon_db_dirver,
+
+  # Cluster Configuration
+  $cluster_enabled = $sp_worker::params::cluster_enabled,
 )
 inherits sp_worker::params {
 
--- modules/sp_worker/manifests/params.pp
+++ modules/sp_worker/manifests/params.pp
@@ -30,4 +30,40 @@ class sp_worker::params {
   # Define the template
   $start_script_template = "bin/${product_profile}.sh"
   $deployment_yaml_template = "conf/${product_profile}/deployment.yaml"
+
+  # -------- deployment.yaml configs --------
+
+  # listenerConfigurations
+  $default_host = '0.0.0.0'
+
+  $msf4j_host = '0.0.0.0'
+  $msf4j_keystore_file = '${carbon.home}/resources/security/wso2carbon.jks'
+  $msf4j_keystore_password = 'wso2carbon'
+  $msf4j_cert_pass = 'wso2carbon'
+
+  # Configuration used for the databridge communication
+  $databridge_keystore_location = '${sys:carbon.home}/resources/security/wso2carbon.jks'
+  $databridge_keystore_password = 'wso2carbon'
+  $binary_data_receiver_hostname = '0.0.0.0'
+
+  # Configuration of the Data Agents - to publish events through databridge
+  $thrift_agent_trust_store = '${sys:carbon.home}/resources/security/client-truststore.jks'
+  $thrift_agent_trust_store_password = 'wso2carbon'
+  $binary_agent_trust_store = '${sys:carbon.home}/resources/security/client-truststore.jks'
+  $binary_agent_trust_store_password = 'wso2carbon'
+
+  # Secure Vault Configuration
+  $securevault_private_key_alias = 'wso2carbon'
+  $securevault_keystore = '${sys:carbon.home}/resources/security/securevault.jks'
+  $securevault_secret_properties_file = '${sys:carbon.home}/conf/${sys:wso2.runtime}/secrets.properties'
+  $securevault_master_key_reader_file = '${sys:carbon.home}/conf/${sys:wso2.runtime}/master-keys.yaml'
+
+  # Datasource Configurations
+  $carbon_db_url = 'jdbc:h2:${sys:carbon.home}/wso2/${sys:wso2.runtime}/database/WSO2_CARBON_DB;DB_CLOSE_ON_EXIT=FALSE;LOCK_TIMEOUT=60000'
+  $carbon_db_username = 'wso2carbon'
+  $carbon_db_password = 'wso2carbon'
+  $carbon_db_dirver = 'org.h2.Driver'
+
+  # Cluster Configuration
+  $cluster_enabled = 'false'
 }
